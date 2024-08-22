b94d5be28f8e9706446aa6c50a2bb28c974e1c8d
NethXXX <1nazedev@gmail.com>
2024-08-22 09:29:37 +0530

    ✎🍔  𝐒ａ𝔫Ⓤù 𝔪∂  💘💢

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: precess.env.ALIVE_IMG || "https://telegra.ph/file/acb5d9378fdf2923a7f49.jpg",
-ALIVE_MSG: precess.env.ALIVE_MSG || "╭────────────◉◉►\n\n│Hey, I'm Sanuu MD\n\n│Created By Mr.Nethmika✨🍂\n\n╰────────────◉◉►\n\n✎🍔  𝐒ａ𝔫Ⓤù 𝔪∂  💘💢",
+ALIVE_MSG: precess.env.ALIVE_MSG || "╭────────────◉◉►\n\n│Hey, I'm Sanuu MD\n\n│Created By Mr.Nethmika✨🍂\n\n╰────────────◉◉►\n\n✎🍔  𝐒ａ𝔫Ⓤù 𝔪∂  💘💢"
 };
